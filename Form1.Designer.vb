0e99b1968e38869456ece7c48d3a4909ad049f69
Joshua McCuistion <joshua.mccuistion@grizzly.com>
2020-11-29 16:04:26 -0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,246 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+
+    Private components As System.ComponentModel.IContainer
+
+
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtR1length = New System.Windows.Forms.TextBox()
+        Me.txtR2length = New System.Windows.Forms.TextBox()
+        Me.txtR3length = New System.Windows.Forms.TextBox()
+        Me.txtR1width = New System.Windows.Forms.TextBox()
+        Me.txtR3width = New System.Windows.Forms.TextBox()
+        Me.txtR2width = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.lbltotal = New System.Windows.Forms.Label()
+        Me.lblmsg = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(106, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(240, 24)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Small House Area Clculator"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(800, 36)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(21, 108)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(155, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "What is Your Name?"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(205, 110)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(189, 20)
+        Me.txtName.TabIndex = 3
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(45, 172)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(61, 16)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Room 1"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(45, 209)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(61, 16)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Room 2"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(45, 248)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(61, 16)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "Room 3"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(158, 155)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(48, 16)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "Length"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(279, 155)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(42, 16)
+        Me.Label8.TabIndex = 8
+        Me.Label8.Text = "Width"
+        '
+        'txtR1length
+        '
+        Me.txtR1length.Location = New System.Drawing.Point(135, 174)
+        Me.txtR1length.Name = "txtR1length"
+        Me.txtR1length.Size = New System.Drawing.Size(96, 20)
+        Me.txtR1length.TabIndex = 9
+        '
+        'txtR2length
+        '
+        Me.txtR2length.Location = New System.Drawing.Point(135, 205)
+        Me.txtR2length.Name = "txtR2length"
+        Me.txtR2length.Size = New System.Drawing.Size(96, 20)
+        Me.txtR2length.TabIndex = 10
+        '
+        'txtR3length
+        '
+        Me.txtR3length.Location = New System.Drawing.Point(135, 244)
+        Me.txtR3length.Name = "txtR3length"
+        Me.txtR3length.Size = New System.Drawing.Size(96, 20)
+        Me.txtR3length.TabIndex = 11
+        '
+        'txtR1width
+        '
+        Me.txtR1width.Location = New System.Drawing.Point(250, 174)
+        Me.txtR1width.Name = "txtR1width"
+        Me.txtR1width.Size = New System.Drawing.Size(96, 20)
+        Me.txtR1width.TabIndex = 12
+        '
+        'txtR3width
+        '
+        Me.txtR3width.Location = New System.Drawing.Point(250, 244)
+        Me.txtR3width.Name = "txtR3width"
+        Me.txtR3width.Size = New System.Drawing.Size(96, 20)
+        Me.txtR3width.TabIndex = 13
+        '
+        'txtR2width
+        '
+        Me.txtR2width.Location = New System.Drawing.Point(250, 205)
+        Me.txtR2width.Name = "txtR2width"
+        Me.txtR2width.Size = New System.Drawing.Size(96, 20)
+        Me.txtR2width.TabIndex = 14
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(48, 315)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(346, 23)
+        Me.Button1.TabIndex = 15
+        Me.Button1.Text = "Calculate the Area of My House"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'lbltotal
+        '
+        Me.lbltotal.AutoSize = True
+        Me.lbltotal.Location = New System.Drawing.Point(167, 393)
+        Me.lbltotal.Name = "lbltotal"
+        Me.lbltotal.Size = New System.Drawing.Size(39, 13)
+        Me.lbltotal.TabIndex = 16
+        Me.lbltotal.Text = "Label9"
+        '
+        'lblmsg
+        '
+        Me.lblmsg.AutoSize = True
+        Me.lblmsg.Location = New System.Drawing.Point(161, 357)
+        Me.lblmsg.Name = "lblmsg"
+        Me.lblmsg.Size = New System.Drawing.Size(45, 13)
+        Me.lblmsg.TabIndex = 17
+        Me.lblmsg.Text = "Label10"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(458, 450)
+        Me.Controls.Add(Me.lblmsg)
+        Me.Controls.Add(Me.lbltotal)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtR2width)
+        Me.Controls.Add(Me.txtR3width)
+        Me.Controls.Add(Me.txtR1width)
+        Me.Controls.Add(Me.txtR3length)
+        Me.Controls.Add(Me.txtR2length)
+        Me.Controls.Add(Me.txtR1length)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "McCuistion Lab 7"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtR1length As TextBox
+    Friend WithEvents txtR2length As TextBox
+    Friend WithEvents txtR3length As TextBox
+    Friend WithEvents txtR1width As TextBox
+    Friend WithEvents txtR3width As TextBox
+    Friend WithEvents txtR2width As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents lbltotal As Label
+    Friend WithEvents lblmsg As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,44 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim strName As String
+
+        Dim intlength1 As Integer
+        Dim intlength2 As Integer
+        Dim intlength3 As Integer
+        Dim intwidth1 As Integer
+        Dim intwidth2 As Integer
+        Dim intwidth3 As Integer
+
+        Dim room1 As Integer
+        Dim room2 As Integer
+        Dim room3 As Integer
+
+        Dim intTotal As Integer
+
+        strName = txtName.Text
+
+        intlength1 = Val(txtR1length.Text)
+        intlength2 = Val(txtR2length.Text)
+        intlength3 = Val(txtR3length.Text)
+        intwidth1 = Val(txtR1width.Text)
+        intwidth2 = Val(txtR2width.Text)
+        intwidth3 = Val(txtR3width.Text)
+
+        room1 = intlength1 + intwidth1
+        room2 = intlength2 + intwidth2
+        room3 = intlength3 + intwidth3
+
+        intTotal = room1 + room2 + room3
+
+        If intTotal < 2000 Then
+            lblmsg.Text = "Ahem," & strName & "this is a SMALL house calculator"
+            lbltotal.Text = "Sorry " & "," & "no answer for you" & "!!"
+        Else
+            lblmsg.Text = "The total area of the house is" & ":"
+            lbltotal.Text = intTotal & "Square Feet"
+        End If
+
+
+    End Sub
+End Class
